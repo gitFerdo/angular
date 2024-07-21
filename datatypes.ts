3f0d9e54c05127d9250ee0e7039b9f1778851b0f
amasha <fernandothushini8@gmail.com>
2024-07-21 22:31:00 +0530

    Data Types
    Number

--- datatypes.ts
+++ datatypes.ts
@@ -1,10 +1,17 @@
 // String
 let lname: string;
-
 lname = "John";
 
 // Capitalizated
 let newName = lname.toUpperCase();
 console.log(newName);
 
+// Number
+let age: number;
+age = 24;
+age = 25.5;
+
+let num = "23";
+
+let result = parseInt(num);
 // lname = 10;
